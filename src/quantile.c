306ee915ba66ab81f81b8c8763c0ffb62a24df2f
Tomas Vondra <tv@fuzzy.cz>
2012-03-26 22:38:13 +0200

    Removed obsolete reference to GPL (replaced by BSD license).

--- src/quantile.c
+++ src/quantile.c
@@ -2,22 +2,6 @@
 * quantile.c - Quantile aggregate function
 * Copyright (C) Tomas Vondra, 2011
 *
-* This program is free software: you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation, either version 3 of the License, or
-* (at your option) any later version.
-*
-* This program is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-* GNU General Public License for more details.
-*
-* You should have received a copy of the GNU General Public License
-* along with this program. If not, see <http://www.gnu.org/licenses/>.
-*
-* TODO Take care of the memory consumption (respect work_mem, flush
-* the data to disk if more space is needed and use merge sort, i.e.
-* something like the ORDER BY uses).
 */
 
 #include <stdio.h>
