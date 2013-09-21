b8ca2b1b7d67a3ff3cd72a15048ea5be125a9dd0
tomas.vondra <tomas.vondra@gooddata.com>
2013-09-21 19:46:31 +0200

    Expanded unit tests, to check NULL handling for all supported data types (not just INT).

--- test/sql/base.sql
+++ test/sql/base.sql
@@ -54,6 +54,7 @@ SELECT quantile(x::numeric, ARRAY[1]) FROM generate_series(1,1000) s(x);
 
 SELECT quantile(x::numeric, ARRAY[0, 0.1, 0.5, 0.75, 1]) FROM generate_series(1,1000) s(x);
 
+-- test of correct NULL handling (skipping with all NULLS)
 CREATE TABLE parent_table (id int);
 CREATE TABLE child_table  (id int, val int);
 
@@ -64,14 +65,25 @@ INSERT INTO child_table  SELECT 6, i FROM generate_series(1,100) s(i);
 INSERT INTO child_table  SELECT 8, i FROM generate_series(1,100) s(i);
 INSERT INTO child_table  SELECT 10, i FROM generate_series(1,100) s(i);
 
--- test of correct NULL handling (skipping with all NULLS)
 SELECT parent_table.id, quantile(child_table.val, array[0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]) FROM parent_table LEFT JOIN child_table USING (id) GROUP BY id ORDER BY id;
 SELECT parent_table.id, quantile(child_table.val, 0.5) FROM parent_table LEFT JOIN child_table USING (id) GROUP BY id ORDER BY id;
 
+SELECT parent_table.id, quantile(child_table.val::bigint, array[0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]) FROM parent_table LEFT JOIN child_table USING (id) GROUP BY id ORDER BY id;
+SELECT parent_table.id, quantile(child_table.val::bigint, 0.5) FROM parent_table LEFT JOIN child_table USING (id) GROUP BY id ORDER BY id;
+
+SELECT parent_table.id, quantile(child_table.val::double precision, array[0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]) FROM parent_table LEFT JOIN child_table USING (id) GROUP BY id ORDER BY id;
+SELECT parent_table.id, quantile(child_table.val::double precision, 0.5) FROM parent_table LEFT JOIN child_table USING (id) GROUP BY id ORDER BY id;
+
+SELECT parent_table.id, quantile(child_table.val::numeric, array[0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]) FROM parent_table LEFT JOIN child_table USING (id) GROUP BY id ORDER BY id;
+SELECT parent_table.id, quantile(child_table.val::numeric, 0.5) FROM parent_table LEFT JOIN child_table USING (id) GROUP BY id ORDER BY id;
+
+-- test of correct NULL handling (skipping with mixed NULL and not-NULL values)
 TRUNCATE child_table;
 INSERT INTO child_table  SELECT i, (CASE WHEN MOD(i,2) = 0 THEN i/2 ELSE NULL END) FROM generate_series(1,200) s(i);
 
--- test of correct NULL handling (skipping with mixed NULL and not-NULL values)
 SELECT quantile(val, array[0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]) FROM (SELECT val FROM child_table ORDER BY id) AS foo;
+SELECT quantile(val::bigint, array[0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]) FROM (SELECT val FROM child_table ORDER BY id) AS foo;
+SELECT quantile(val::double precision, array[0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]) FROM (SELECT val FROM child_table ORDER BY id) AS foo;
+SELECT quantile(val::numeric, array[0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]) FROM (SELECT val FROM child_table ORDER BY id) AS foo;
 
 ROLLBACK;
